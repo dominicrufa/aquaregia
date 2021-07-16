8ebe9364741a28b13d94b52b4f3862f53f3b60a3
dominicrufa <dominic.rufa@gmail.com>
2021-07-16 15:22:33 -0400

    create

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a mixture of nitric acid and hydrochloric acid.',
+    author='dominic rufa',
+    license='MIT',
+)
